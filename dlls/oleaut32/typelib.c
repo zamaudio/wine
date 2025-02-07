0eb1a896b0dac248dea425df8b535b64bd12b6cd
Edward O'Callaghan <edward@antitrust.cc>
2025-02-07 17:28:43 +1100

    dlls/oleaut32: Scope indexer to loops in fn[Is|Find]Name()
    Signed-off-by: Edward O'Callaghan <edward@antitrust.cc>

--- dlls/oleaut32/typelib.c
+++ dlls/oleaut32/typelib.c
@@ -5051,7 +5051,6 @@ static HRESULT WINAPI ITypeLib2_fnIsName(
 	BOOL *pfName)
 {
     ITypeLibImpl *This = impl_from_ITypeLib2(iface);
-    int tic;
 
     TRACE("%p, %s, %#lx, %p.\n", iface, debugstr_w(szNameBuf), lHashVal, pfName);
 
@@ -5059,7 +5058,7 @@ static HRESULT WINAPI ITypeLib2_fnIsName(
         return E_INVALIDARG;
 
     *pfName=TRUE;
-    for(tic = 0; tic < This->TypeInfoCount; ++tic){
+    for(int tic = 0; tic < This->TypeInfoCount; ++tic){
         ITypeInfoImpl *pTInfo = This->typeinfos[tic];
 
         if(!lstrcmpiW(TLB_get_bstr(pTInfo->Name), szNameBuf)) {
@@ -5110,7 +5109,6 @@ static HRESULT WINAPI ITypeLib2_fnFindName(
 	UINT16 *found)
 {
     ITypeLibImpl *This = impl_from_ITypeLib2(iface);
-    int tic;
     UINT count = 0;
 
     TRACE("%p, %s %#lx, %p, %p, %p.\n", iface, debugstr_w(szNameBuf), lHashVal, ppTInfo, memid, found);
@@ -5119,7 +5117,7 @@ static HRESULT WINAPI ITypeLib2_fnFindName(
         return E_INVALIDARG;
 
     // TODO: factor out common impl with fnIsName().
-    for(tic = 0; count < *found && tic < This->TypeInfoCount; ++tic) {
+    for(int tic = 0; count < *found && tic < This->TypeInfoCount; ++tic) {
         ITypeInfoImpl *pTInfo = This->typeinfos[tic];
 
         if(!lstrcmpiW(TLB_get_bstr(pTInfo->Name), szNameBuf)) {
@@ -5143,6 +5141,7 @@ static HRESULT WINAPI ITypeLib2_fnFindName(
         }
 
         continue;
+
 ITypeLib2_fnFindName_exit:
         ITypeInfo2_AddRef(&pTInfo->ITypeInfo2_iface);
         ppTInfo[count] = (ITypeInfo *)&pTInfo->ITypeInfo2_iface;
